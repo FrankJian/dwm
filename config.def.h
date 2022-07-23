4bd3f8439be4d92559793a45f5e8cd1110895618
j3xie <jian-frank.xie@nokia-sbell.com>
2022-07-23 20:14:01 +0800

    17 patch , add tcl layer patch

--- config.def.h
+++ config.def.h
@@ -52,11 +52,14 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 0; /* 1 will force focus on the fullscreen window */
 
+#include "tcl.c"
+
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "|||",      tcl},
 };
 
 /* key definitions */
