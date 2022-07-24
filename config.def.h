787661ebb56cbefa07604133a82c10d6bae46919
j3xie <jian-frank.xie@nokia-sbell.com>
2022-07-24 08:11:23 +0800

    remove aspectresize patch

--- config.def.h
+++ config.def.h
@@ -106,8 +106,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask|ControlMask, XK_j,      aspectresize,   {.i = +24} },
-	{ MODKEY|ShiftMask|ControlMask, XK_k,      aspectresize,   {.i = -24} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
