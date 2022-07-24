da63e5589639fe3012000da55bda375f53bddff6
j3xie <jian-frank.xie@nokia-sbell.com>
2022-07-24 12:17:15 +0800

    20 patch , add status2d

--- config.def.h
+++ config.def.h
@@ -8,7 +8,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const int user_bh            = 4;        /* 2 is the default spacing around the bar's font */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "SauceCodePro Nerd Font:size=15" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
