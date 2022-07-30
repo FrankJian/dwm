ebf911ab8965b04c40cde801673369c5a2114164
j3xie <jian-frank.xie@nokia-sbell.com>
2022-07-30 14:48:46 +0800

    format the config.def.h

--- config.def.h
+++ config.def.h
@@ -1,31 +1,34 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 1; /* border pixel of windows */
-static const Gap default_gap = {.isgap = 1, .realgap = 10, .gappx = 10};
-static const unsigned int snap = 32; /* snap pixel */
-static const int showbar = 1;        /* 0 means no bar */
-static const int topbar = 1;         /* 0 means bottom bar */
-static const Bool viewontag = True;  /* Switch view on tag switch */
-static const int user_bh =
-    4; /* 2 is the default spacing around the bar's font */
-static const char *fonts[] = {"SauceCodePro Nerd Font:size=13"};
-static const unsigned int baralpha = 0xd0;
-static const unsigned int borderalpha = OPAQUE;
-static const char *colors[][3] = {
-    /*               fg         bg         border   */
-    [SchemeNorm] = {"#bbbbbb", "#333333", "#444444"},
-    [SchemeSel] = {"#ffffff", "#37474f", "#42A5F5"},
-    [SchemeHid] = {"#dddddd", NULL, NULL},
-    [SchemeUnderline] = {"#7799AA", "#7799AA", "#7799AA"},
+static const unsigned int borderpx     = 1;       /* border pixel of windows */
+static const Gap          default_gap  = {
+    .isgap = 1, 
+    .realgap = 10, 
+    .gappx = 10
 };
-static const unsigned int alphas[][3] = {
-    /*               fg      bg        border     */
-    [SchemeNorm] = {OPAQUE, baralpha, borderalpha},
-    [SchemeSel] = {OPAQUE, baralpha, borderalpha},
+static const unsigned int snap         = 32;      /* snap pixel */
+static const int          showbar      = 1;       /* 0 means no bar */
+static const int          topbar       = 1;       /* 0 means bottom bar */
+static const Bool         viewontag    = True;    /* Switch view on tag switch */
+static const int          user_bh      = 4;       /* 2 is the default spacing around the bar's font */
+static const char         *fonts[]     = {"SauceCodePro Nerd Font:size=13"};
+static const unsigned int baralpha     = 0xd0;
+static const unsigned int borderalpha  = OPAQUE;
+static const char         *colors[][3] = {
+    /*                  fg            bg          border   */
+    [SchemeNorm]   =    {"#bbbbbb",   "#333333",  "#444444"},
+    [SchemeSel]    =    {"#ffffff",   "#37474f",  "#42A5F5"},
+    [SchemeHid]    =    {"#dddddd",   NULL,       NULL},
+    [SchemeUnderline] = {"#7799AA",   "#7799AA",  "#7799AA"},
+};
+static const unsigned int alphas[][3]  = {
+    /*                  fg            bg          border     */
+    [SchemeNorm]   =    {OPAQUE,      baralpha,   borderalpha},
+    [SchemeSel]    =    {OPAQUE,      baralpha,   borderalpha},
 };
 
-static const char *const autostart[] = {
+static const char *const autostart[]   = {
     "dwmbar.sh", NULL,
     //"picom", "/home/j3xie/.config/picom/picom.conf", NULL,
     //"nm-applet", NULL,
@@ -51,27 +54,27 @@ static const Rule rules[] = {
      *	WM_NAME(STRING) = title
      */
     /* class      instance    title       tags mask     isfloating   monitor */
-    {"Gimp", NULL, NULL, 0, 1, -1},
-    {"Firefox", NULL, NULL, 1 << 8, 0, -1},
+    {  "Gimp",    NULL,       NULL,       0,            1,           -1},
+    {  "Firefox", NULL,       NULL,       1 << 8,       0,           -1},
 };
 
 /* layout(s) */
-static const float mfact = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster = 1;    /* number of clients in master area */
-static const int resizehints =
+static const float  mfact   = 0.55; /* factor of master area size [0.05..0.95] */
+static const int    nmaster = 1;    /* number of clients in master area */
+static const int    resizehints =
     1; /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen =
+static const int    lockfullscreen =
     0; /* 1 will force focus on the fullscreen window */
 
 #include "tcl.c"
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
-    {"﬿",  tile}, /* first entry is default */ 
-    {"﩯",  grid},
-    {"><>", NULL}, /* no layout function means floating behavior */
-    {"[M]", monocle},
-    {"|||", tcl},
+    {  "﬿",       tile}, /* first entry is default */ 
+    {  "﩯",       grid},
+    {  "><>",     NULL}, /* no layout function means floating behavior */
+    {  "[M]",     monocle},
+    {  "|||",     tcl},
 };
 
 /* key definitions */
@@ -89,58 +92,65 @@ static const Layout layouts[] = {
     }
 
 /* commands */
-static const char *termcmd[] = {"st", NULL};
-static const char *trayercmd[] = {"t-toggle.sh", NULL};
+static const char *termcmd[]       = {"st", NULL};
+static const char *trayercmd[]     = {"t-toggle.sh", NULL};
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = {"st", "-t",     scratchpadname,
                                       "-g", "120x34", NULL};
 
 static Key keys[] = {
-    /* modifier                     key        function        argument */
-    {MODKEY, XK_Return, spawn, {.v = termcmd}},
-    {MODKEY, XK_grave, togglescratch, {.v = scratchpadcmd}},
-    {MODKEY, XK_b, togglebar, {0}},
-    {MODKEY, XK_r, spawn, SHCMD("rofi -show drun")},
-    {MODKEY | ShiftMask, XK_t, spawn, {.v = trayercmd}},
-    {MODKEY | ShiftMask, XK_j, rotatestack, {.i = +1}},
-    {MODKEY | ShiftMask, XK_k, rotatestack, {.i = -1}},
-    {MODKEY | ControlMask, XK_space, focusmaster, {0}},
-    {MODKEY, XK_j, focusstackvis, {.i = +1}},
-    {MODKEY, XK_k, focusstackvis, {.i = -1}},
-    {MODKEY | ControlMask, XK_j, focusstackhid, {.i = +1}},
-    {MODKEY | ControlMask, XK_k, focusstackhid, {.i = -1}},
-    {MODKEY, XK_i, incnmaster, {.i = +1}},
-    {MODKEY, XK_d, incnmaster, {.i = -1}},
-    {MODKEY, XK_h, setmfact, {.f = -0.05}},
-    {MODKEY, XK_l, setmfact, {.f = +0.05}},
-    {MODKEY | ShiftMask, XK_Return, zoom, {0}},
-    {MODKEY, XK_Tab, view, {0}},
-    {MODKEY, XK_q, killclient, {0}},
-    {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
-    {MODKEY, XK_g, setlayout, {.v = &layouts[1]}},
-    {MODKEY, XK_m, setlayout, {.v = &layouts[2]}},
-    {MODKEY, XK_space, setlayout, {0}},
-    {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
-    {MODKEY, XK_f, togglefullscreen, {0}},
-    {MODKEY, XK_0, view, {.ui = ~0}},
-    {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
-    {MODKEY, XK_comma, focusmon, {.i = -1}},
-    {MODKEY, XK_period, focusmon, {.i = +1}},
-    {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
-    {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
-    {MODKEY, XK_minus, setgaps, {.i = -5}},
-    {MODKEY, XK_equal, setgaps, {.i = +5}},
-    {MODKEY | ShiftMask, XK_minus, setgaps, {.i = GAP_RESET}},
-    {MODKEY | ShiftMask, XK_equal, setgaps, {.i = GAP_TOGGLE}},
-    {MODKEY, XK_Left, viewtoleft, {0}},
-    {MODKEY, XK_Right, viewtoright, {0}},
-    {MODKEY | ShiftMask, XK_Left, tagtoleft, {0}},
-    {MODKEY | ShiftMask, XK_Right, tagtoright, {0}},
-    {MODKEY, XK_s, show, {0}},
-    {MODKEY | ShiftMask, XK_s, hide, {0}},
-    TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
-        TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
-            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
+    /* modifier                        key            function           argument */
+    {  MODKEY,                         XK_Return,     spawn,             {.v = termcmd}},
+    {  MODKEY,                         XK_grave,      togglescratch,     {.v = scratchpadcmd}},
+    {  MODKEY,                         XK_b,          togglebar,         {0}},
+    {  MODKEY,                         XK_r,          spawn,             SHCMD("rofi -show drun")},
+    {  MODKEY | ShiftMask,             XK_t,          spawn,             {.v = trayercmd}},
+    {  MODKEY | ShiftMask,             XK_j,          rotatestack,       {.i = +1}},
+    {  MODKEY | ShiftMask,             XK_k,          rotatestack,       {.i = -1}},
+    {  MODKEY | ControlMask,           XK_space,      focusmaster,       {0}},
+    {  MODKEY,                         XK_j,          focusstackvis,     {.i = +1}},
+    {  MODKEY,                         XK_k,          focusstackvis,     {.i = -1}},
+    {  MODKEY | ControlMask,           XK_j,          focusstackhid,     {.i = +1}},
+    {  MODKEY | ControlMask,           XK_k,          focusstackhid,     {.i = -1}},
+    {  MODKEY,                         XK_i,          incnmaster,        {.i = +1}},
+    {  MODKEY,                         XK_d,          incnmaster,        {.i = -1}},
+    {  MODKEY,                         XK_h,          setmfact,          {.f = -0.05}},
+    {  MODKEY,                         XK_l,          setmfact,          {.f = +0.05}},
+    {  MODKEY | ShiftMask,             XK_Return,     zoom,              {0}},
+    {  MODKEY,                         XK_Tab,        view,              {0}},
+    {  MODKEY,                         XK_q,          killclient,        {0}},
+    {  MODKEY,                         XK_t,          setlayout,         {.v = &layouts[0]}},
+    {  MODKEY,                         XK_g,          setlayout,         {.v = &layouts[1]}},
+    {  MODKEY,                         XK_m,          setlayout,         {.v = &layouts[2]}},
+    {  MODKEY,                         XK_space,      setlayout,         {0}},
+    {  MODKEY | ShiftMask,             XK_space,      togglefloating,    {0}},
+    {  MODKEY,                         XK_f,          togglefullscreen,  {0}},
+    {  MODKEY,                         XK_0,          view,              {.ui = ~0}},
+    {  MODKEY | ShiftMask,             XK_0,          tag,               {.ui = ~0}},
+    {  MODKEY,                         XK_comma,      focusmon,          {.i = -1}},
+    {  MODKEY,                         XK_period,     focusmon,          {.i = +1}},
+    {  MODKEY | ShiftMask,             XK_comma,      tagmon,            {.i = -1}},
+    {  MODKEY | ShiftMask,             XK_period,     tagmon,            {.i = +1}},
+    {  MODKEY,                         XK_minus,      setgaps,           {.i = -5}},
+    {  MODKEY,                         XK_equal,      setgaps,           {.i = +5}},
+    {  MODKEY | ShiftMask,             XK_minus,      setgaps,           {.i = GAP_RESET}},
+    {  MODKEY | ShiftMask,             XK_equal,      setgaps,           {.i = GAP_TOGGLE}},
+    {  MODKEY,                         XK_Left,       viewtoleft,        {0}},
+    {  MODKEY,                         XK_Right,      viewtoright,       {0}},
+    {  MODKEY | ShiftMask,             XK_Left,       tagtoleft,         {0}},
+    {  MODKEY | ShiftMask,             XK_Right,      tagtoright,        {0}},
+    {  MODKEY,                         XK_s,          show,              {0}},
+    {  MODKEY | ShiftMask,             XK_s,          hide,              {0}},
+    TAGKEYS(XK_1, 0)
+    TAGKEYS(XK_2, 1)
+    TAGKEYS(XK_3, 2)
+    TAGKEYS(XK_4, 3)
+    TAGKEYS(XK_5, 4)
+    TAGKEYS(XK_6, 5)
+    TAGKEYS(XK_7, 6)
+    TAGKEYS(XK_8, 7)
+    TAGKEYS(XK_9, 8)
+    {MODKEY | ShiftMask,               XK_q, quit, {0}},
     {MODKEY | ControlMask | ShiftMask, XK_q, quit, {1}},
 };
 
