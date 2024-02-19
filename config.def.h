9796befc39e9402e216731836137ec44d0249865
j3xie <jian-frank.xie@nokia-sbell.com>
2024-02-19 13:07:31 +0800

    Merge: config.def.h: make keys and buttons const

--- config.def.h
+++ config.def.h
@@ -100,7 +100,7 @@ static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = {"st", "-t",     scratchpadname,
                                       "-g", "120x34", NULL};
 
-static Key keys[] = {
+static const Key keys[] = {
     /* modifier                        key                         function           argument */
     {  MODKEY,                         XK_Return,                  spawn,             {.v = termcmd}},
     {  MODKEY,                         XK_grave,                   togglescratch,     {.v = scratchpadcmd}},
@@ -166,7 +166,7 @@ static Key keys[] = {
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
  * ClkClientWin, or ClkRootWin */
-static Button buttons[] = {
+static const Button buttons[] = {
     /* click                event mask      button          function         argument */
     {ClkLtSymbol,           0,              Button1,        setlayout,       {0}},
     {ClkLtSymbol,           0,              Button3,        setlayout,       {.v = &layouts[2]}},
